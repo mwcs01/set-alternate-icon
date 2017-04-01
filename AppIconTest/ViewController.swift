b5e676544032f4e92937357a936b330bec2352b2
James Baughman <mwcs01@me.com>
2017-03-31 22:52:47 -0500

    Initial Commit

---
+++ AppIconTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppIconTest
+//
+//  Created by Jim Baughman on 3/31/17.
+//  Copyright © 2017 Midwest Computer Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
